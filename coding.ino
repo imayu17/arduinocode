160588e97a98aa818fa83c7f3489ce9b5c1fe08e
imayu17 <82795330+imayu17@users.noreply.github.com>
2021-04-26 21:44:07 +0700

    Add files via upload

--- coding.ino
+++ coding.ino
@@ -2,8 +2,8 @@
 
   int SENSOR_H2S = A0;
   float ppm = 0.0; //erhitungan dengan RUMUS
-  int data = 0;
-  int Rload = 40000; // yg bagus RL = 10K ~ 47 K
+  int Sensor_Value = 0;
+  int Rload = 20000; // yg bagus RL = 10K ~ 47 K
   long r0 = 2000000; //didapatkan dari rumus
   float a = 24.27050317; //Dari Power Regression Calculator
   float b = -1.110798221;
@@ -11,7 +11,7 @@
   float maxRsR0 = 2* pow((0.01/a),1/b); //0.01 s.d 1000 ppm
   #define LED D1
   #define INDICATOR D5
-  #define BUZZER D2
+  #define BUZZER D7
   
 
 void setup() 
@@ -28,37 +28,41 @@ void setup()
 }
 
 void loop() 
-{
-                                                                                                                                                                                                                                                                                                                                                                 , HIGH);
-  int Sensor_Value = analogRead(SENSOR_H2S);
+ {
+  Sensor_Value = analogRead(SENSOR_H2S);
   Serial.print("Temp : "); Serial.println(Sensor_Value);
-  long rS = ((2048.0 * Rload) / data ) - Rload; //data = dari sensor H2S
+  long rS = ((1023.0 * Rload) / Sensor_Value ) - Rload;
   float rSr0 = (float)rS / (float) r0;
   
   if (rSr0 < maxRsR0 && rSr0 > minRsR0) //Perumusan pembacaan real H2S
   {
-    float ppm = a * pow((float)rS/ (float)r0, b);  
+    float ppma = a * pow((float)rS/ (float)r0, b);  
+    ppm = ppma;
     Serial.print("H2S:");
     Serial.println(ppm);
   }
    else 
     {
-      Serial.print("H2S: OUT OF RANGE");
+      Serial.println("H2S: OUT OF RANGE");
     }
-
-  if (ppm < 4.6)
+  Serial.println();
+  
+  if (ppm < 10)
   {
     Serial.println ("ZONA AMAN");
     digitalWrite(BUZZER, LOW); // Buzzer Off
     digitalWrite(INDICATOR, LOW); // LED off
     delay(1000);
   }
-    else if (ppm > 4.6 /*&&  ppm < 10*/) 
+    else //if (ppm > 10 /*&&  ppm < 10*/) 
     { 
      Serial.println ("ZONA WASPADA");
      digitalWrite(BUZZER, HIGH); // Buzzer On
      digitalWrite(INDICATOR, HIGH); // LED on
-     delay(1000);
+     delay(500);
+     digitalWrite(BUZZER, LOW); // Buzzer On
+     digitalWrite(INDICATOR, LOW); // LED on
+     delay(500);
     }
 
    /* if (Sensor_Value )
@@ -71,5 +75,5 @@ void loop()
         digitalWrite(LED, LOW);
         digitalWrite(BUZZER, LOW);
       } */
-   delay(100);
+ 
 } 
